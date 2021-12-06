db7c5f206c9f27b9228482dafefef6c38c5d12a2
shivam saini <shivamsainier98@gmail.com>
2021-12-06 12:08:52 +0530

    Add Styling

--- src/App.js
+++ src/App.js
@@ -3,23 +3,25 @@ import {Route,Switch,Redirect} from "react-router-dom"
 import AllQuotes from "./components/Pages/AllQuotes";
 import NewQuote from "./components/Pages/NewQuote"
 import QuoteDetails from "./components/Pages/QuoteDetails";
+import Layout from "./components/layout/Layout"
 
 function App() {
   return (
     <div>
+      <Layout>
       <Switch>
         <Route path="/quotes" exact><AllQuotes /></Route>
         <Route path="/" exact>
           <Redirect to="/quotes"></Redirect>
         </Route>
-        <Route path="/quotes/:quoteId" exact>
+        <Route path="/quotes/:quoteId">
           <QuoteDetails />
         </Route>
         <Route path="/new-quote" exact>
           <NewQuote />
         </Route>
-
       </Switch>
+      </Layout>
 
     </div>
   );
--- src/components/Pages/QuoteDetails.js
+++ src/components/Pages/QuoteDetails.js
@@ -1,12 +1,17 @@
 import React from 'react'
 import {useParams} from "react-router-dom"
+import {Route} from "react-router-dom"
+import Comments from "../comments/Comments"
 
 function QuoteDetails() {
     const params=useParams();
     return (
         <div>
             Quote Details
-            <h2>{params.quoteId}</h2>
+           
+            <Route path={`/quotes/${params.quoteId}/comments`} exact>
+                <Comments />
+            </Route>
         </div>
     )
 }
